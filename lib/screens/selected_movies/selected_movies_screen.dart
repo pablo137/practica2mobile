c9fbcf632badcb730ebf2bdca0e68eee47693ef8
pablo137 <p48102611@gmail.com>
2024-04-06 01:42:35 -0400

    price success ended

--- lib/screens/selected_movies/selected_movies_screen.dart
+++ lib/screens/selected_movies/selected_movies_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:practica2mobile/screens/success/success_screen.dart';
 
-class SelectedMoviesScreen extends StatelessWidget {
+class SelectedMoviesScreen extends StatefulWidget {
   final List<Map<String, dynamic>> selectedMovies;
 
   SelectedMoviesScreen({required this.selectedMovies});
+
+  @override
+  _SelectedMoviesScreenState createState() => _SelectedMoviesScreenState();
+}
+
+class _SelectedMoviesScreenState extends State<SelectedMoviesScreen> {
+  Map<int, int> _selectedQuantities =
+      {}; // Mapa para almacenar la cantidad seleccionada para cada película
+  final double _moviePrice = 20.0; // Precio de cada película
+  double _totalPrice = 0.0; // Precio total de todas las películas seleccionadas
+
+  @override
+  void initState() {
+    super.initState();
+    _updateTotalPrice(); // Inicializar el precio total al inicio
+  }
+
+  void _updateTotalPrice() {
+    double totalPrice = 0.0;
+    _selectedQuantities.forEach((movieId, quantity) {
+      totalPrice += quantity * _moviePrice;
+    });
+    setState(() {
+      _totalPrice = totalPrice;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,96 +42,84 @@ class SelectedMoviesScreen extends StatelessWidget {
         children: [
           Expanded(
             child: ListView.builder(
-              itemCount: selectedMovies.length,
+              itemCount: widget.selectedMovies.length,
               itemBuilder: (context, index) {
-                final movie = selectedMovies[index];
-                return CustomMovieTile(
-                  movie: movie,
-                ); // Usar un ListTile personalizado para mostrar la película
+                final movie = widget.selectedMovies[index];
+                final movieId = movie['id']
+                    as int; // Se asume que 'id' es el identificador único de la película
+                final quantity = _selectedQuantities[movieId] ??
+                    0; // Obtener la cantidad seleccionada para esta película
+                final totalPrice = quantity *
+                    _moviePrice; // Calcular el precio total para esta película
+
+                return ListTile(
+                  leading: SizedBox(
+                    width: 100,
+                    height: 150,
+                    child: Image.network(
+                      'https://image.tmdb.org/t/p/w200${movie['poster_path']}',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  title: Text(movie['title']),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(movie['overview'] ?? ''),
+                      SizedBox(height: 8),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                if (quantity > 0) {
+                                  _selectedQuantities[movieId] =
+                                      quantity - 1; // Decrementar la cantidad
+                                  _updateTotalPrice(); // Actualizar el precio total
+                                }
+                              });
+                            },
+                            icon: Icon(Icons.remove),
+                          ),
+                          Text('$quantity'),
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _selectedQuantities[movieId] =
+                                    quantity + 1; // Incrementar la cantidad
+                                _updateTotalPrice(); // Actualizar el precio total
+                              });
+                            },
+                            icon: Icon(Icons.add),
+                          ),
+                          Text(
+                            'Precio total: \$${totalPrice.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
               },
             ),
           ),
           ElevatedButton(
             onPressed: () {
-              // Navegar a la pantalla "Success" al hacer clic en el botón "Comprar"
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => SuccessScreen()),
+                MaterialPageRoute(
+                  builder: (context) => SuccessScreen(totalPrice: _totalPrice),
+                ),
               );
             },
             style: ElevatedButton.styleFrom(
-              // Establecer el estilo del botón
-              backgroundColor: Colors.green, // Cambiar el color de fondo del botón a verde
+              backgroundColor: Colors.green,
             ),
             child: Text('Comprar'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class CustomMovieTile extends StatefulWidget {
-  final Map<String, dynamic> movie;
-
-  const CustomMovieTile({Key? key, required this.movie}) : super(key: key);
-
-  @override
-  _CustomMovieTileState createState() => _CustomMovieTileState();
-}
-
-class _CustomMovieTileState extends State<CustomMovieTile> {
-  int _quantity = 0;
-  final double _moviePrice = 20.0; // Precio de la película
-
-  @override
-  Widget build(BuildContext context) {
-    double totalPrice = _quantity * _moviePrice; // Calcular el precio total
-
-    return ListTile(
-      leading: SizedBox(
-        width: 100, // Aumentar el ancho de la imagen
-        height: 150, // Ajustar la altura de la imagen
-        child: Image.network(
-          'https://image.tmdb.org/t/p/w200${widget.movie['poster_path']}',
-          fit: BoxFit.cover,
-        ),
-      ),
-      title: Text(widget.movie['title']),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(widget.movie['overview'] ?? ''),
-          SizedBox(height: 8),
-          Row(
-            children: [
-              IconButton(
-                onPressed: () {
-                  setState(() {
-                    if (_quantity > 0) {
-                      _quantity--; // Reducir la cantidad
-                    }
-                  });
-                },
-                icon: Icon(Icons.remove),
-              ),
-              Text('$_quantity'), // Mostrar la cantidad actual
-              IconButton(
-                onPressed: () {
-                  setState(() {
-                    _quantity++; // Aumentar la cantidad
-                  });
-                },
-                icon: Icon(Icons.add),
-              ),
-              Text(
-                'Precio total: \$${totalPrice.toStringAsFixed(2)}', // Mostrar el precio total
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue,
-                ),
-              ),
-            ],
           ),
         ],
       ),
--- lib/screens/success/success_screen.dart
+++ lib/screens/success/success_screen.dart
@@ -1,7 +1,10 @@
-
 import 'package:flutter/material.dart';
 
 class SuccessScreen extends StatelessWidget {
+  final double totalPrice;
+
+  SuccessScreen({required this.totalPrice});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,9 +12,19 @@ class SuccessScreen extends StatelessWidget {
         title: Text('Success'),
       ),
       body: Center(
-        child: Text(
-          '¡Compra realizada con éxito!',
-          style: TextStyle(fontSize: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '¡Compra realizada con éxito!',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Precio total: \$${totalPrice.toStringAsFixed(2)}',
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
         ),
       ),
     );
